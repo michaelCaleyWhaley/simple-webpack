e844c234969d1c6b8c04b16accfec90f285dcf2d
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2020-11-08 11:40:31 +0000

    correct CLS

--- src/index.js
+++ src/index.js
@@ -11,4 +11,8 @@ setTimeout(() => {
     dots: true,
     arrows: false,
   });
+
+  document
+    .getElementsByClassName("carousel-wrap")[0]
+    .classList.add("carousel-wrap--active");
 }, 300);
